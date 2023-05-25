571f31075430e458a8b24bbbc721575a83e6a747
Agnieszka Majmurek <majmurek.agnieszka@gmail.com>
2023-05-25 17:55:07 +0200

    fix

--- src/components/WeatherDetails/WeatherDetails.js
+++ src/components/WeatherDetails/WeatherDetails.js
@@ -5,25 +5,24 @@ const WeatherDetails = ({ data, isDataCorrect }) => {
 			{isDataCorrect ? (
 				<>
 					<h2>
-						Dane dla <span className='value'>{data?.name}</span>
+						Dane dla <span className='value'>{data.name}</span>
 					</h2>
 					<p>
-						Temperatura: <span className='value'>{data?.main?.temp}</span>
+						Temperatura: <span className='value'>{data.main.temp}</span>
 					</p>
 					<p>
-						Wilgotność powietrza: <span className='value'>{data?.main?.humidity}</span>
+						Wilgotność powietrza: <span className='value'>{data.main.humidity}</span>
 					</p>
 					<p>
-						Ciśnienie: <span className='value'>{data?.maino?.pressure}</span>
+						Ciśnienie: <span className='value'>{data.main.pressure}</span>
 					</p>
 					<p>
-						Pogoda ogólnie:<span className='value'>{data?.weather[0]?.description}</span>
+						Pogoda ogólnie:<span className='value'>{data.weather[0].description}</span>
 					</p>
 				</>
 			) : (
 				<span className='value'>Wpisane miasto nie istnieje</span>
 			)}
-
 		</div>
 	);
 };
